d386bb4097e9b8c735219abefbf0490e6ba36209
drewrasm <drewrasm@gmail.com>
2020-04-30 10:04:03 -0600

    include twilio

--- django_lightningkite/notifications/channels/twilio_channel.py
+++ django_lightningkite/notifications/channels/twilio_channel.py
@@ -1,10 +1,36 @@
 from .channel import Channel
-
+from twilio.rest import Client
 
 class TwilioChannel(Channel):
 
     def send(notifiable, notification):
         """
         Send the Given Notifiction
+
+        questions:
+        - will we want to use the twilio rest api?
+        - how generic will this be? do we need to include being able to text images and such? 
+        
+        items of interest:
+        - phone numbers
+            - phone number that will come from the env
+            - phone number we will send a message to
+        
+        - message body
+
+        - we can even use a status callback to know whether or not it failed or was sent
+
         """
-        pass
+
+        client = Client(settings.TWILIO_SID, settings.TWILIO_TOKEN)
+
+        message_kwargs = notification.to_sms()
+
+        '''
+            the message kwargs need the following arguments 
+            - body
+            - from_
+            - to
+        '''
+        message = client.messages.create(message_kwargs)
+
